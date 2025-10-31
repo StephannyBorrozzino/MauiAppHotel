4788426f16b3207771e32f071f16ab6e20e4ee5c
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2025-10-31 18:00:09 -0300

    Upgrade

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,7 +1,38 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+            new Quarto
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+            },
+            new Quarto
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+            },
+             new Quarto
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+            }
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,13 +2,46 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
-	public ContratacaoHospedagem()
-	{
-		InitializeComponent();
-	}
+    App PropriedadesApp;
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = DateTime.Now.AddMonths(1);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+    }
 
     private async void Button_sobre(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Views.Sobre());
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Views.HospedagemContratada());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkin.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkin.MaximumDate = data_selecionada_checkin.AddMonths(6);
     }
 }
